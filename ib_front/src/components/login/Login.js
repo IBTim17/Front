c33840f70077b19bf2185c7422332f4c8278c58f
Jana Nikolic <jananikolic25@gmail.com>
2023-05-05 23:01:12 +0200

    Added radio button for forgot password

--- ib_front/src/components/login/Login.js
+++ ib_front/src/components/login/Login.js
@@ -1,11 +1,14 @@
 import "./Login.css";
 import React, { useState } from "react";
-import ReactModal from 'react-modal';
+import ReactModal from "react-modal";
 
 function Login() {
   const [isOpen, setIsOpen] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [resource, setResource] = useState("Email");
+  const [resValue, setResValue] = useState("");
+  const emailPhoneNumPlh = "";
 
   function handleEmailChange(event) {
     setEmail(event.target.value);
@@ -14,82 +17,138 @@ function Login() {
   function handlePasswordChange(event) {
     setPassword(event.target.value);
   }
-  
+
+  function handleRecourceChange(event) {
+    setResValue(event.target.value);
+  }
+
+  const onOptionChange = (e) => {
+    setResource(e.target.value);
+  };
+
+  function onCloseModal() {
+    setIsOpen(false);
+    setResValue("");
+  }
+
+  function sendCode(event) {
+    event.preventDefault();
+    
+  }
+
+  function login(event) {
+    event.preventDefault();
+    
+  }
+
   return (
     <div className="container-login">
       <div className="col-sm-6 welcome-back">
         <div className="row">
-          <br/>
+          <br />
           <p className="motto">Welcome back to CertifyHub</p>
         </div>
         <div className="row">
-          <p className="message">You were missed! Welcome back and let's pick up right where we left off. First time visiting us?</p>
+          <p className="message">
+            You were missed! Welcome back and let's pick up right where we left
+            off. First time visiting us?
+          </p>
           {/* <Link to="/register" className="btn btn-primary">Create account.</Link> */}
         </div>
-        <div className="row">
-          {/* <img src="..\..\assets\logo.png"> */}
-        </div>
+        <div className="row">{/* <img src="..\..\assets\logo.png"> */}</div>
       </div>
       <div className="col-sm-6 line">
         <div className="row form">
-          <br/>
+          <br />
           <p className="sign-in">Sign in</p>
         </div>
-        <div className="row form">
-          <div className="input-box">
-            <span className="details">Email</span>
-            <input 
-              type="text" 
-              placeholder="bob@ross.com" 
-              value={email}
-              onChange={handleEmailChange}
-              required/>
+        <form onSubmit={login}>
+          <div className="row form">
+            <div className="input-box">
+              <span className="details">Email</span>
+              <input
+                type="text"
+                placeholder="bob@ross.com"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div className="row form">
-          <div className="input-box">
-            <span className="details">Password</span>
-            <input 
-              type="password" 
-              placeholder="********" 
-              value={password}
-              onChange={handlePasswordChange}
-              required/>
+          <div className="row form">
+            <div className="input-box">
+              <span className="details">Password</span>
+              <input
+                type="password"
+                placeholder="********"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
           </div>
-        </div>
-        <a className="right" onClick={() => setIsOpen(true)}>Forgot password?</a>
-        <div className="row">
-          <div className="button">
-            <input type="submit" value="Sign in"/>
+          <a className="right" onClick={() => setIsOpen(true)}>
+            Forgot password?
+          </a>
+          <div className="row">
+            <div className="button">
+              <input type="submit" value="Sign in" />
+            </div>
           </div>
-        </div>
+        </form>
       </div>
       <ReactModal
         isOpen={isOpen}
         contentLabel="Example Modal"
-        onRequestClose={() => setIsOpen(false)}
-        >
-          <div className="row form" style={{marginTop:'3em'}}>
-            <div className="input-box">
-              <span className="details">Email or PhoneNumber</span>
-              <input 
-                style={{width:'95%'}}
-                type="text" 
-                placeholder="bob@ross.com or +381651234567" 
-                value={email}
-                onChange={handleEmailChange}
-                required/>
+        onRequestClose={onCloseModal}
+      >
+        <div className="row form" style={{ marginTop: "3em" }}>
+          <form onSubmit={sendCode}>
+            <div className="inline-radio">
+              <span className="radio-gap">
+                <input
+                  type="radio"
+                  name="resource"
+                  value="Email"
+                  id="email"
+                  checked={resource === "Email"}
+                  onChange={onOptionChange}
+                />
+                <label htmlFor="email">Email</label>
+              </span>
+              <span className="radio-gap">
+                <input
+                  type="radio"
+                  name="resource"
+                  value="Phone Number"
+                  id="phoneNumber"
+                  checked={resource === "Phone Number"}
+                  onChange={onOptionChange}
+                />
+                <label htmlFor="medium">Phone Number</label>
+              </span>
             </div>
-            <div style={{marginTop:'3em'}}>
+
+            <div className="input-box">
+              <span className="details">{resource}</span>
+              <input
+                style={{ width: "95%" }}
+                type="text"
+                placeholder={emailPhoneNumPlh}
+                value={resValue}
+                onChange={handleRecourceChange}
+                required
+              />
+            </div>
+            <div style={{ marginTop: "3em" }}>
               <div className="button">
-                <input type="submit" value="Send Code"/>
+                <input type="submit" value="Send Code" />
               </div>
             </div>
-          </div>
-        
+          </form>
+        </div>
       </ReactModal>
     </div>
   );
-  
 }
 export default Login;
